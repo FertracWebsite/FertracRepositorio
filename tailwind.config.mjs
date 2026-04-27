685076582e38214e5724e7bdcf62dcba16754fe1
FertracHost <asistente_diseno@fertrac.com>
2026-04-27 10:57:01 -0500

    Blog adelantado

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,31 +1,31 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	
-	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue }'
-	],
-	
-	theme: {
-		extend: {
-			fontFamily : {
-
-				"GTWalsheim" : ["GT Walsheim", "sans-serif"],
-			},
-
-			colors : {
-				"naranja" : "#f2790c",
-				"azul" : "#04297e",
-				"naranja-opaco": "#F4780B1A",
-				"azul-opaco": "#04287C0D",
-			},
-			screens :{
-				"sm" : "360px",
-				"md" : "768px",      
-				"lg" : "1080px",
-				"xl" : "1536px",
-				"2xl" : "2000px",	
-			},
-			
-		},
-	},
+    content: [
+        './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+    ],
+    theme: {
+        extend: {
+            fontFamily : {
+                "GTWalsheim" : ["GT Walsheim", "sans-serif"],
+            },
+            colors : {
+                "naranja" : "#f2790c",
+                "azul" : "#04297e",
+                "naranja-opaco": "#F4780B1A",
+                "azul-opaco": "#04287C0D",
+            },
+            screens :{
+                "sm" : "360px",
+                "md" : "768px",      
+                "lg" : "1080px",
+                "xl" : "1536px",
+                "2xl" : "2000px",   
+            },
+        },
+    },
+    plugins: [
+        typography, 
+    ],
 }
