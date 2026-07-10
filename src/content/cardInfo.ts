2f5e277cc275453a55f6c5967c62d4007b47330f
Edwin Camilo Roberto Peña <eroberto@fertrac.col>
2026-07-10 17:51:36 -0500

    Arreglos pagina especialidades motor

---
+++ src/content/cardInfo.ts
@@ -0,0 +1,42 @@
+export const products = [
+  {
+    image: "/productos/casquete.webp",
+    title: "CASQUETE BANCADA STD, MEDIA LUNAS",
+    reference: "CUMMINS ISM",
+  },
+  {
+    image: "/productos/otro.webp",
+    title: "OTRO PRODUCTO",
+    reference: "REFERENCIA",
+  },
+    {
+    image: "/productos/casquete.webp",
+    title: "CASQUETE BANCADA STD, MEDIA LUNAS",
+    reference: "CUMMINS ISM",
+  },
+  {
+    image: "/productos/otro.webp",
+    title: "OTRO PRODUCTO",
+    reference: "REFERENCIA",
+  },
+    {
+    image: "/productos/casquete.webp",
+    title: "CASQUETE BANCADA STD, MEDIA LUNAS",
+    reference: "CUMMINS ISM",
+  },
+  {
+    image: "/productos/otro.webp",
+    title: "OTRO PRODUCTO",
+    reference: "REFERENCIA",
+  },
+    {
+    image: "/productos/casquete.webp",
+    title: "CASQUETE BANCADA STD, MEDIA LUNAS",
+    reference: "CUMMINS ISM",
+  },
+  {
+    image: "/productos/otro.webp",
+    title: "OTRO PRODUCTO",
+    reference: "REFERENCIA",
+  },
+];
